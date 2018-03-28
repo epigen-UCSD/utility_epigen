aec140c77aea371014894ac6ea236013526216af
frank@tscc <zhangc518@gmail.com>
2018-03-28 15:38:20 -0700

    fix bugs in  tagtobdg -2

--- tagToBdg.sh
+++ tagToBdg.sh
@@ -3,8 +3,6 @@
 # check commands: slopBed, bedGraphToBigWig and bedClip
 
 which bedtools &>/dev/null || { echo "bedtools not found! Download bedTools: <http://code.google.com/p/bedtools/>"; exit 1; }
-which bedGraphToBigWig &>/dev/null || { echo "bedGraphToBigWig not found! Download: <http://hgdownload.cse.ucsc.edu/admin/exe/>"; exit 1; }
-which bedClip &>/dev/null || { echo "bedClip not found! Download: <http://hgdownload.cse.ucsc.edu/admin/exe/>"; exit 1; }
 
 # end of checking
 
@@ -18,8 +16,7 @@ fi
 tag=$1 # input
 chrsz=$2 # chrsz
 
-
-prefix=${bdg%.bdg}
+prefix=${bdg%.tagAlign}
 bdg=${prefix}.bdg
 
 bedtools genomecov -i $tag -bg -g $chrsz > $bdg
